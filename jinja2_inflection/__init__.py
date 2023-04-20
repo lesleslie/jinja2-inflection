64760e3c9575ae3fce212f394339a11871f3a1bd
lesleslie <les@wedgwoodwebworks.com>
2023-04-20 03:05:39 -0700

    add extension

---
+++ jinja2_inflection/__init__.py
@@ -0,0 +1,34 @@
+from inflection import camelize
+from inflection import dasherize
+from inflection import humanize
+from inflection import ordinal
+from inflection import ordinalize
+from inflection import parameterize
+from inflection import pluralize
+from inflection import singularize
+from inflection import tableize
+from inflection import titleize
+from inflection import transliterate
+from inflection import underscore
+
+from jinja2.ext import Extension
+
+
+class InflectionExtension(Extension):
+    def __init__(self, environment):
+        super(InflectionExtension, self).__init__(environment)
+        inflection_filters = dict(
+            camelize=camelize,
+            dasherize=dasherize,
+            humanize_inflection=humanize,
+            ordinal=ordinal,
+            ordinalize=ordinalize,
+            parameterize=parameterize,
+            pluralize=pluralize,
+            singularize=singularize,
+            tableize=tableize,
+            titleize=titleize,
+            transliterate=transliterate,
+            underscore=underscore,
+        )
+        environment.filters.update(inflection_filters)
